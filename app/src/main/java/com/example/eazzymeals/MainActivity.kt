4f0374315fc9231c67e8cfc146213ff2fdddf090
st10467866 <st10467866@vcconnect.edu.za>
2025-04-02 10:25:51 +0200

    Added referencing

--- app/src/main/java/com/example/eazzymeals/MainActivity.kt
+++ app/src/main/java/com/example/eazzymeals/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlin.system.exitProcess
 
+//References:
+//https://eur03.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.canva.com%2Fdesign%2FDAGi2Bf8IUM%2Fs7Gu_Sn0SgclhG4edsm7XQ%2Fedit%3Futm_content%3DDAGi2Bf8IUM%26utm_campaign%3Ddesignshare%26utm_medium%3Dlink2%26utm_source%3Dsharebutton&data=05%7C02%7Cst10467866%40vcconnect.edu.za%7C394981933b834c8e81bd08dd71bf164c%7Ce10c8f44f469448fbc0dd781288ff01b%7C0%7C0%7C638791787749307820%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=AoPi3dpDJuw0sDB1kIlHkfhGJh%2FPLuxXllt81JC1pwg%3D&reserved=0
+
 
 class MainActivity : AppCompatActivity() {
     private var timeOfDayInput: EditText? = null
